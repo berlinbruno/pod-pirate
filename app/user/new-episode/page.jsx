3afbf4d2984ec47fc4f41d98d43a7bb31c6365c2
Berlin Bruno <berlinbruno112@gmail.com>
2024-08-17 12:38:55 +0530

    1.0

--- app/user/new-episode/page.jsx
+++ app/user/new-episode/page.jsx
@@ -2,7 +2,7 @@ import EpisodeForm from "@/components/form/EpisodeForm";
 import { cookies } from "next/headers";
 
 export const metadata = {
-  title: "Post New Episode",
+  title: "New Episode",
 };
 
 export default async function NewEpisodePage() {
